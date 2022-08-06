4a3c5a2cb431a80e879c7d8f6455ebdbe3e7d40f
Orie Steele <orie@transmute.industries>
2022-08-06 10:04:35 -0500

    chore: fix did key + did web examples

--- services/UniversalResolver.js
+++ services/UniversalResolver.js
@@ -1,6 +1,22 @@
 import axios from "axios";
 
+
+import didWeb from '@transmute/did-web';
+import * as didKey from '@transmute/did-key.js';
+
 const resolve = async (did) => {
+
+
+  if (did.startsWith('did:web')){
+    const didDocument = await didWeb.resolve(did);
+    return  { didDocument, didResolutionMetadata: {}};
+  }
+
+  if (did.startsWith('did:key')){
+    const {didDocument} = await didKey.resolve(did);
+    return  { didDocument, didResolutionMetadata: {}};
+  }
+
   const response = await axios.get(
     `https://dev.uniresolver.io/1.0/identifiers/${did}`,
     {
